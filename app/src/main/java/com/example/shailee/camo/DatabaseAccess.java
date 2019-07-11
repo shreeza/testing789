62b37731a06570c0fc7293efc46a81ac57c4d818
shreeza <shreeja970@gmail.com>
2019-07-11 21:42:53 +0545

    test commit

---
+++ app/src/main/java/com/example/shailee/camo/DatabaseAccess.java
@@ -0,0 +1,109 @@
+package com.example.shailee.camo;
+
+
+//class to make database access
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+
+public class DatabaseAccess extends AppCompatActivity {
+    private DatabaseOpenHelper openHelper;
+    private SQLiteDatabase db;
+    private static DatabaseAccess instance;
+    Cursor c= null;
+    Cursor d=null;
+    ImageView img;
+
+
+    //private constructor creation so that this class wont be accessed or instantiated by any other classses
+    private DatabaseAccess(Context context){
+        this.openHelper=new DatabaseOpenHelper(context);
+    }
+    //to return the single instance of the database
+    public static DatabaseAccess getInstance(Context context){
+        if(instance==null){
+            instance=new DatabaseAccess(context);
+        }
+        return instance;
+    }
+    //to open the connection to the database
+
+    public void open(){
+        this.db=openHelper.getWritableDatabase();
+    }
+    //to close the connection
+
+    public void close(){
+        if(db!=null){
+           this.db.close();
+        }
+
+    }
+    public String getData(String commonName) {
+
+        String str;
+        c = db.rawQuery("Select scientific_name, family, genus,benefits from leaf where common_name = ?", new String[]{commonName});
+        StringBuffer buffer = new StringBuffer();
+
+        //d = db.rawQuery("Select images from leaf where common_name=?", new String[]{commonName});
+        while (c.moveToNext()) {
+            String scientificName = c.getString(0);
+            String Family = c.getString(1);
+            String Genus = c.getString(2);
+            String benefits = c.getString(3);
+
+
+
+            buffer.append("Scientific name : "  + scientificName + "\n" + "Family Name : " + Family + "\n" + "Genus : " + Genus + "\n" + "Benefits : "+ benefits);
+        }
+//        while(d.moveToNext()){
+//            img.findViewById(R.id.leafdisplay);
+//            byte[] buff=new byte[1024];
+//
+//            img.setImageBitmap(bitmap);
+//
+//
+//        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        return buffer.toString();
+
+    }
+    public byte[] getImage(String commonName) {
+        byte[] data = null;
+        d = db.rawQuery("SELECT images FROM leaf WHERE common_name = ?", new String[]{commonName});
+        d.moveToFirst();
+        while (!d.isAfterLast()) {
+            data = d.getBlob(0);
+            break;  // Assumption: name is unique
+        }
+        d.close();
+        return data;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(DatabaseAccess.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/shailee/camo/MainActivity.java
@@ -0,0 +1,270 @@
+package com.example.shailee.camo;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Blob;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Button button;
+    Button buttonOfQuery;
+    EditText editText;
+    TextView textView;
+    ImageView img;
+
+    ImageView imageView;
+    Toolbar toolbar;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    DrawerLayout drawerLayout;
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+
+
+
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        simulateDayNight(/* DAY */ 0);
+        Element adsElement = new Element();
+        adsElement.setTitle("Advertise with us");
+
+
+
+
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.
+                navigation_drawer_close);//This requires adding of two strings which is  added in strings.xml
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+         //snap the image wala button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                   startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+
+            }
+        });
+
+
+        }
+
+
+
+
+
+
+//navigation drawer ko items selection..
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+//about us view ko lagi case 1
+            case R.id.nav_con:
+                Element adsElement = new Element();
+                adsElement.setTitle("Advertise with us");
+                //getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,new AboutPage(this)).commit();
+                View aboutPage = new AboutPage(this)
+                        .isRTL(false)
+                        .setImage(R.drawable.dummy_image)
+                        .addItem(new Element().setTitle("Version 3.0"))
+                        .addItem(adsElement)
+                        .addGroup("Connect with us")
+                        .addEmail("shreejashrestha15@gmail.com")
+                        .addWebsite("http://medyo.github.io/")
+                        .addFacebook("shreezastha")
+                        .addTwitter(" ")
+                        .addYoutube("UCdPQtdWIsg7_pi4mrRu46vA")
+                        .addPlayStore("com.ideashower.readitlater.pro")
+                        .addInstagram("shreeja.stha")
+                        .addGitHub("shreeza")
+                        .addItem(getCopyRightsElement())
+                        .create();
+
+                 //setContentView(aboutPage);
+
+                setContentView(aboutPage);
+                break;
+            case R.id.opt_search:
+                setContentView(R.layout.search);
+
+                buttonOfQuery=(Button)findViewById(R.id.search_button);
+                editText=(EditText)findViewById(R.id.search_plant);
+                textView=(TextView)findViewById(R.id.result);
+                img=(ImageView)findViewById(R.id.leafdisplay);
+                //arkai button
+                buttonOfQuery.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DatabaseAccess databaseAccess=DatabaseAccess.getInstance(getApplicationContext());
+                        databaseAccess.open();
+
+                        //getting string value from edit text
+                        String n=editText.getText().toString();
+                        String result=databaseAccess.getData(n);
+                        SpannableStringBuilder fancySentence = new SpannableStringBuilder(result);
+                        fancySentence.setSpan(new android.text.style.StyleSpan(Typeface.BOLD), 0, 15, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                       fancySentence.setSpan(new android.text.style.StyleSpan(Typeface.BOLD), 36, 48, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        fancySentence.setSpan(new android.text.style.StyleSpan(Typeface.BOLD), 60, 66, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        fancySentence.setSpan(new android.text.style.StyleSpan(Typeface.BOLD), 75, 84, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                        textView.setText(fancySentence);
+
+                     //   textView.setText(result);
+
+                       // byte[] b=databaseAccess.getImage(n);
+//                        BitmapFactory.Options options = new BitmapFactory.Options();
+//                        Bitmap bitmap = BitmapFactory.decodeByteArray(b, 0, b.length, options);
+                        //img.setImageBitmap(bitmap);
+
+                        byte[] data=databaseAccess.getImage(n);
+                        Bitmap image=toBitmap(data);
+                        img.setImageBitmap(image);
+
+
+
+
+                        databaseAccess.close();
+
+
+
+                    }
+                });
+                break;
+
+
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    public static Bitmap toBitmap(byte[] image) {
+        return BitmapFactory.decodeByteArray(image, 0, image.length);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+       if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+          drawerLayout.closeDrawer(GravityCompat.START);
+     } else {
+           super.onBackPressed();
+           finish();
+       }
+
+    }
+    String currentPhotoPath;
+
+    Element getCopyRightsElement() {
+        Element copyRightsElement = new Element();
+        final String copyrights = String.format(getString(R.string.copy_right), Calendar.getInstance().get(Calendar.YEAR));
+        copyRightsElement.setTitle(copyrights);
+        copyRightsElement.setIconDrawable(R.drawable.about_icon_copy_right);
+        copyRightsElement.setIconTint(R.color.about_item_text_color);
+        copyRightsElement.setIconNightTint(android.R.color.white);
+        copyRightsElement.setGravity(Gravity.CENTER);
+        copyRightsElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, copyrights, Toast.LENGTH_SHORT).show();
+            }
+        });
+        return copyRightsElement;
+    }
+    void simulateDayNight(int currentSetting) {
+        final int DAY = 0;
+        final int NIGHT = 1;
+        final int FOLLOW_SYSTEM = 3;
+
+        int currentNightMode = getResources().getConfiguration().uiMode
+                & Configuration.UI_MODE_NIGHT_MASK;
+        if (currentSetting == DAY && currentNightMode != Configuration.UI_MODE_NIGHT_NO) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (currentSetting == NIGHT && currentNightMode != Configuration.UI_MODE_NIGHT_YES) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES);
+        } else if (currentSetting == FOLLOW_SYSTEM) {
+            AppCompatDelegate.setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
+    }
+
+
+//to create image file...
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    //to save into image gallery...
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+}
